648cd5bd0774b57fc6f78d69a41c517ef7694f72
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-10-04 21:17:20 +0300

    feat: added backend data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from 'react';
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <h1 className='underline text-3xl'>App</h1>
   )
 }
 
