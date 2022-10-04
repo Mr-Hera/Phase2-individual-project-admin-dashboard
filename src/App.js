ff82515ed431ffc5e6ae20f653504ce13bc951a8
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-10-04 23:20:58 +0300

    feat: defining side-bar routes

--- src/App.js
+++ src/App.js
@@ -35,6 +35,35 @@ const App = () => {
               Navbar
             </div>
           </div>
+          <div>
+            <Routes>
+              {/* Dashboard */}
+              <Route path="/" element="ECommerce" />
+              <Route path="/ecommerce" element="ECommerce" />
+
+              {/* Pages */}
+              <Route path="/orders" element="Orders" />
+              <Route path="/employees" element="Employees" />
+              <Route path="/customers" element="Customers" />
+              
+              {/* Apps */}
+              <Route path="/calendar" element="Calendar" />
+              <Route path="/kanban" element="Kanban" />
+              <Route path="/editor" element="Editor" />
+              <Route path="/color-picker" element="ColorPicker" />
+
+              {/* Charts */}
+              <Route path="/line" element="Line" />
+              <Route path="/area" element="Area" />
+              <Route path="/bar" element="Bar" />
+              <Route path="/pie" element="Pie" />
+              <Route path="/financial" element="Financial" />
+              <Route path="/color-mapping" element="ColorMapping" />
+              <Route path="/pyramid" element="Pyramid" />
+              <Route path="/stacked" element="Stacked" />
+
+            </Routes>
+          </div>
         </div>
       </BrowserRouter>
     </div>
