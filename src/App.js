15423e35aa0c85bdce9bfa45ba17cc07e7adcd8f
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-10-04 23:06:13 +0300

    feat: side-bar navigation(s) setup

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 
 const App = () => {
   
-  const activeMenu = true;
+  const activeMenu = false;
   
   return (
     <div>
@@ -28,6 +28,13 @@ const App = () => {
               Sidebar
             </div>
           )}
+          <div className={
+            `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? "md:ml-72" : "flex-2"}`
+          }>
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              Navbar
+            </div>
+          </div>
         </div>
       </BrowserRouter>
     </div>
