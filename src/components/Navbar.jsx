913333e1361c2a35d2261dc089cfe87728e82e0a
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-10-07 01:08:20 +0300

    feat: creating state for navbar items & passing to navbar component via context provider

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const NavButton = ({title, customFunc, icon, color, dotColor}) => (
 
 const Navbar = () => {
 
-  const {activeMenu, setActiveMenu} = useStateContext();
+  const {activeMenu, setActiveMenu, isClicked, setIsClicked} = useStateContext();
 
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -12,9 +12,10 @@ const initialState = {
 export const ContextProvider = ({children}) => {
 
     const [activeMenu, setActiveMenu] = useState(true);
+    const [isClicked, setIsClicked] = useState(initialState);
 
     return (
-        <StateContext.Provider value={{activeMenu, setActiveMenu}}>
+        <StateContext.Provider value={{activeMenu, setActiveMenu, isClicked, setIsClicked}}>
             {children}
         </StateContext.Provider>
     )
