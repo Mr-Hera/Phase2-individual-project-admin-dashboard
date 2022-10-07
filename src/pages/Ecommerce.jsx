ecd9f6cb29048e2f713e1588cf3fa1859eb06923
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-10-07 14:24:28 +0300

    feat: adding line chart for tracking budget & expense data

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -69,6 +69,9 @@ const Ecommerce = () => {
                 </p>
                 <p className="text-gray-500 mt-1">Expense</p>
               </div>
+              <div className="mt-5">
+                <SparkLine currentColor="blue" id="line-sparkline" type="line" height="80px" width="250px" data={SparklineAreaData} color="blue" />
+              </div>
             </div>
           </div>
         </div>
