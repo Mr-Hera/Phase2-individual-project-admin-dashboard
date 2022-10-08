2317f944da9aa36952989f46329081533d981fbc
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-10-08 18:31:33 +0300

    feat: adding pagination and sorting to orders datatable

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -7,12 +7,13 @@ const Orders = () => {
   return (
     <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Orders" />
-      <GridComponent id="grid comp" dataSource={ordersData}>
+      <GridComponent id="grid comp" dataSource={ordersData} allowPaging allowSorting>
         <ColumnsDirective>
           {ordersGrid.map((item, index) => (
             <ColumnDirective key={index} {...item} />
           ))}
         </ColumnsDirective>
+        <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, Edit, PdfExport]} />
       </GridComponent>
     </div>
   )
