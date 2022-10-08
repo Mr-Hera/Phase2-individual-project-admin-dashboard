5fb88935f949e4879541023e333d618b4ca68c81
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-10-08 18:58:59 +0300

    feat: adding customers datatable with edit, sort & filter functionality

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, columnsDirective, columnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter} from '@syncfusion/ej2-react-grids';
+import { customersData, customersGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Customers = () => {
   return (
-    <div>
-      
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Customers" />
+      <GridComponent dataSource={customersData} allowPaging allowSorting toolbar={['Delete']} editSSettings={{allowDeleting: true, allowEditing: true}} width="auto" >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Selection, Edit, Sort, Filter ,Toolbar]} />
+      </GridComponent>
     </div>
   )
 }
