b9daa75fe8a0cc5d66a43a44734ecefbb3ff1247
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-10-08 19:45:35 +0300

    fixing declaration typo bug in customers component

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GridComponent, columnsDirective, columnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter} from '@syncfusion/ej2-react-grids';
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter} from '@syncfusion/ej2-react-grids';
 import { customersData, customersGrid } from '../data/dummy';
 import { Header } from '../components';
 
