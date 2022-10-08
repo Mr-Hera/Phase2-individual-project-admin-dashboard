8db6df1464c35eb1d8245969add6595de6cec52c
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-10-08 16:52:53 +0300

    fixed SparkLine graph bug

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -4,15 +4,15 @@ import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-re
 const SparkLine = ({id, height, width, color, data, type, currentColor}) => {
   return (
     <SparklineComponent 
-      id={id} height={height} width={width} lineWidth={1} valueType="Numeric" fill={color} border={{color: currentColor, width: 2}} dataSource={data} xName="x" yName="y" type={type}
+      id={id} height={height} width={width} LineWidth={1} valueType="Numeric" fill={color} border={{ color: currentColor, width: 2}} dataSource={data} xName="x" yName="yval" type={type}
       tooltipSettings={{
         visible: true,
-        format: `${x} : data ${y}`,
+        format: '${x} : data ${yval}',
         trackLineSettings: {
           visible: true
         }
       }}
-    >
+    > 
       <Inject services={[SparklineTooltip]} />
     </SparklineComponent>
   )
--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -70,7 +70,7 @@ const Ecommerce = () => {
                 <p className="text-gray-500 mt-1">Expense</p>
               </div>
               <div className="mt-5">
-                <SparkLine currentColor="blue" id="line-sparkline" type="line" height="80px" width="250px" data={SparklineAreaData} color="blue" />
+                <SparkLine currentColor="blue" id="line-sparkline" type="Line" height="80px" width="250px" data={SparklineAreaData} color="blue" />
               </div>
             </div>
           </div>
