506368896511d415b8ca77d928c0c28ae61f42d9
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-10-08 17:50:41 +0300

    feat: adding stacked chart component to ecommerce page

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -72,6 +72,12 @@ const Ecommerce = () => {
               <div className="mt-5">
                 <SparkLine currentColor="blue" id="line-sparkline" type="Line" height="80px" width="250px" data={SparklineAreaData} color="blue" />
               </div>
+              <div className="mt-10">
+                <Button color="white" bgColor="blue" text="Download Report" border="10px" />
+              </div>
+            </div>
+            <div>
+              <Stacked width="320px" height="360px" />
             </div>
           </div>
         </div>
