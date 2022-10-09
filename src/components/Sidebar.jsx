d0ac0aae955de474bacdf44f2437a3ee0e0ee27d
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-10-09 23:32:07 +0300

    fixed sidebar bug that highlights current page/component on display

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,7 @@ import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
 
-  const {activeMenu, setActiveMenu, screenSize} = useStateContext();
+  const {activeMenu, setActiveMenu, screenSize, currentColor} = useStateContext();
 
   const handleCloseSidebar = () => {
     if(activeMenu && screenSize <= 900) {
@@ -40,7 +40,7 @@ const Sidebar = () => {
                 {item.title}
               </p>
               {item.links.map(link => (
-                <NavLink to={`/${link.name}`} key={link.name} onClick={handleCloseSidebar} className={({isActive}) => isActive ? activeLink : normalLink}>
+                <NavLink to={`/${link.name}`} key={link.name} onClick={handleCloseSidebar} style={({isActive}) => ({backgroundColor: isActive ? currentColor : ''})} className={({isActive}) => isActive ? activeLink : normalLink}>
                   {link.icon}
                   <span className="capitalize">
                     {link.name}
