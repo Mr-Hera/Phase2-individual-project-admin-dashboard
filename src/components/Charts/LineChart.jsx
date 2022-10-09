f23fcb501b1ac6ec895eab294ea0e4a73d30643d
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-10-10 00:28:58 +0300

    fixing SparkLine component resizing bug

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -6,6 +6,7 @@ import { useStateContext } from '../../contexts/ContextProvider';
 const LineChart = () => {
 
   const {currentMode} = useStateContext();
+  
   return (
     <ChartComponent id="line-chart" height="420px" primaryXAxis={LinePrimaryXAxis} primaryYAxis={LinePrimaryYAxis} chartArea={{border: {width: 0}}} tooltip={{enable: true}} background={currentMode === 'Dark' ? '#33373E' : '#fff'}>
       <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -4,7 +4,7 @@ import { Header, LineChart } from '../../components';
 const Line = () => {
   return (
     <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
-      <Header categor="Chart" title="Inflation Rate" />
+      <Header category="Chart" title="Inflation Rate" />
       <div className="w-full">
         <LineChart />
       </div>
