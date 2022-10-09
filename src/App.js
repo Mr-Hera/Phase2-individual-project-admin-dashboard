b2515ed8490b4bb70927fb30bd23680dec9f9cf9
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-10-09 23:38:19 +0300

    feat: added dark/light mode functionality

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ import { useStateContext } from './contexts/ContextProvider';
 
 const App = () => {
   
-  const {activeMenu, themeSettings, setThemeSettings} = useStateContext();
+  const {activeMenu, themeSettings, setThemeSettings, currentMode} = useStateContext();
   
   return (
-    <div>
+    <div className={currentMode === 'Dark' ? 'dark' : ''}>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: '1000'}}>
             <TooltipComponent content="Settings" position="top">
-              <button type="button" className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white" onClick={() => setThemeSettings(true)} style={{background:'blue', borderRadius: '50%'}}>
+              <button type="button" className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white" onClick={() => setThemeSettings(true)} style={{background: currentColor, borderRadius: '50%'}}>
                 <FiSettings />
               </button>
             </TooltipComponent>
@@ -32,7 +32,7 @@ const App = () => {
             </div>
           )}
           <div className={
-            `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? "md:ml-72" : "flex-2"}`
+            `dark:bg-main-dark-bg bg-main-bg min-h-screen w-full ${activeMenu ? "md:ml-72" : "flex-2"}`
           }>
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
               <Navbar />
